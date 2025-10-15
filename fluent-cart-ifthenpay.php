28b7948911841d9556bfefbf7d7d943924af6cac
Marco Almeida <marcoalmeida@webdados.pt>
2025-10-15 10:07:39 +0100

    First commit

---
+++ fluent-cart-ifthenpay.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Plugin Name:          ifthenpay Multibanco for FluentCart
+ * Plugin URI:
+ * Description:          Short description here
+ * Version:              0.1
+ * Author:               Naked Cat Plugins (by Webdados)
+ * Author URI:           https://nakedcatplugins.com
+ * Text Domain:          fluent-cart-ifthenpay
+ * Requires at least:    6.7
+ * Tested up to:         6.9
+ * Requires PHP:         7.4
+ * License:              GPLv3
+ **/
+
+namespace NakedCatPlugins\FluentCartIfthenpay;
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+// Set the plugin's main file constant
+define( 'NAKEDCATPLUGINS_FLUENTCART_IFTHENPAY_FILE', __FILE__ );
+
+/**
+ * Initialize the plugin.
+ *
+ * This function serves as the main entry point for the plugin. It ensures
+ * the class is loaded and returns the singleton instance.
+ *
+ * @return Lang_Attribute_Blocks The singleton instance of the plugin class.
+ */
+function init_plugin() {
+	// Load the main class
+	// require_once 'includes/class-lang-attribute-blocks.php';
+	// Return the singleton instance
+	return FluentCartIfthenpay::get_instance();
+}
+
+// Initialize the plugin
+// init_plugin();
+
+/* If you're reading this you must know what you're doing ;-) Greetings from sunny Portugal! */
---
+++ readme.txt
@@ -0,0 +1,35 @@
+=== ifthenpay Multibanco for FluentCart  ===
+Contributors: nakedcatplugins, webdados
+Tags:
+Requires at least: 6.7
+Tested up to: 6.9
+Requires PHP: 7.4
+Stable tag: 0.1
+License: GPLv3
+License URI: https://www.gnu.org/licenses/gpl-3.0.html
+
+Small descrition here
+
+== Description ==
+
+Long description here
+
+== Features ==
+* 
+
+== Screenshots ==
+
+1. 
+
+== Installation ==
+
+1. 
+
+== Frequently Asked Questions ==
+
+= How can I report security bugs? =
+
+You can report security bugs through the Patchstack Vulnerability Disclosure Program. The Patchstack team helps validate, triage, and handle any security vulnerabilities. [Report a security vulnerability.]( https://patchstack.com/database/vdp/ce04f590-44d9-45f3-9411-9028e87d4725 )
+
+== Changelog ==
+
